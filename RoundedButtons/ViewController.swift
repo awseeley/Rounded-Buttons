0a00b2d738bb7fdb78bd80b7c073321958d0cfd3
Andrew Seeley <awseeley@gmail.com>
2015-07-01 22:28:18 +1000

    Rounded Buttons
    Rounded Buttons

--- RoundedButtons/ViewController.swift
+++ RoundedButtons/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var btnRounded: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //btnRounded.layer.borderColor = UIColor.blueColor().CGColor // Set border color
+        btnRounded.layer.borderColor = UIColor(red: 81/255, green: 159/255, blue: 243/255, alpha: 1).CGColor
+        btnRounded.layer.borderWidth = 1 // Set border width
+        btnRounded.layer.cornerRadius = 5 // Set border radius (Make it curved, increase this for a more rounded button
+        btnRounded.layer.backgroundColor = UIColor.greenColor().CGColor
+        
     }
 
     override func didReceiveMemoryWarning() {
