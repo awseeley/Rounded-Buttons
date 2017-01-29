3f814773bd8f781f85b7faca660cd9acb3fefb05
Andrew Seeley <awseeley@gmail.com>
2017-01-29 17:43:04 +1100

    Updated for Swift 3
    Updated for Swift 3

--- RoundedButtons/ViewController.swift
+++ RoundedButtons/ViewController.swift
@@ -17,11 +17,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //btnRounded.layer.borderColor = UIColor.blueColor().CGColor // Set border color
-        btnRounded.layer.borderColor = UIColor(red: 81/255, green: 159/255, blue: 243/255, alpha: 1).CGColor
+        btnRounded.layer.borderColor = UIColor.blue.cgColor
+        btnRounded.layer.borderColor = UIColor(red: 81/255, green: 159/255, blue: 243/255, alpha: 1).cgColor
         btnRounded.layer.borderWidth = 1 // Set border width
         btnRounded.layer.cornerRadius = 5 // Set border radius (Make it curved, increase this for a more rounded button
-        btnRounded.layer.backgroundColor = UIColor.greenColor().CGColor
+        btnRounded.layer.backgroundColor = UIColor.green.cgColor
         
     }
 
